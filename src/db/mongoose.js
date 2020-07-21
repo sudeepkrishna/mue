e6940323a7405cb26818db324e12820d3c5aad11
Sudeep Krishna <sudeepkrishna@Sudeeps-MacBook-Pro.local>
2020-07-21 13:44:51 +0530

    commit before pilot deploy

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 //CONNECT TO A DB
-mongoose.connect("mongodb://127.0.0.1:27017/chat-app", {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
